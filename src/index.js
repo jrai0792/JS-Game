3887894aa1e11e8d7f05d84973edf9e0139866dc
Jyoti <jyotikiran12@gmail.com>
2021-04-08 11:49:53 +0530

    Add resize function

--- src/index.js
+++ src/index.js
@@ -5,30 +5,54 @@ import GameEnd from './scenes/GameEnd';
 import GameMenu from './scenes/gamemenu';
 import Player from './players/Player'
 
-var player;
+let game;
 
-const config = {
-  type: Phaser.AUTO,
-  parent: 'content',
-  width: 1430,
-  height: 770,
-  // zoom: 2,
-  pixelArt : true,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: {x:0,  y: 0 }
-    }
-  },
-  scene: [
-    GameMenu,
-    GameStart,
-    GameMain,
-    GameEnd
-  ]
+let resize = () => {
+  const canvas = document.querySelector('canvas');
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const windowRatio = width/height;
+  const gameRatio = game.config.width / game.config.height;
+
+  if(windowRatio<gameRatio){
+    
+    canvas.style.width = `${width}px`;
+    canvas.style.height = `${width / gameRatio}px`;
+  }else {
+    canvas.style.width = `${height * gameRatio}px`;
+    canvas.style.height = `${height}px`;
+  }
 };
 
-const game = new Phaser.Game(config);
+window.onload = function () {
+
+  const config = {
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: 1430,
+    height: 770,
+    // zoom: 2,
+    pixelArt : true,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: {x:0,  y: 0 }
+      }
+    },
+    scene: [
+      GameMenu,
+      GameStart,
+      GameMain,
+      GameEnd
+    ],
+    backgroundColor: 	0x66ffd9
+  };
+  
+  game = new Phaser.Game(config);
+  window.focus();
+  resize();
+  window.addEventListener('resize', resize, false);
+};
 
 
 // game.scene.add(GameMenu);

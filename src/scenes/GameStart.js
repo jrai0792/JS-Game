b433d1c13a477f38f31ae1d3cafeb1d97018a339
Jyoti <jyotikiran12@gmail.com>
2021-04-13 23:03:47 +0530

    Update assets folder

--- src/scenes/GameStart.js
+++ src/scenes/GameStart.js
@@ -7,12 +7,12 @@ class GameStart extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('sky', '../src/assets/sky.png');
-    this.load.image('ground', '../src/assets/platform.png');
-    this.load.image('star', '../src/assets/star.png');
-    this.load.image('bomb', '../src/assets/bomb.png');
+    this.load.image('sky', '../dist/assets/sky.png');
+    this.load.image('ground', '../dist/assets/platform.png');
+    this.load.image('star', '../dist/assets/star.png');
+    this.load.image('bomb', '../dist/assets/bomb.png');
     this.load.spritesheet('dude',
-      '../src/assets/dude.png',
+      '../dist/assets/dude.png',
       {
         frameWidth: 32,
         frameHeight: 48,

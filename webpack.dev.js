151f6b905d46723d6c0daf859904193c6b6f0550
Jyoti <jyotikiran12@gmail.com>
2021-04-14 18:24:18 +0530

    Add webpack dev server details

---
+++ webpack.dev.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 5501,
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml|mp3|wav)$/i,
+        use: 'asset/resource',
+      },
+    ],
+  },
+};
